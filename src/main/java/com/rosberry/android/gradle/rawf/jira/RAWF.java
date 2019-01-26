dc1dead82a396a18c172af9d4a22f188e8f41c61
Alexey Korshun <lehakorshun@gmail.com>
2019-01-26 15:08:36 +0600

    create getReleaseNotes method

--- src/main/java/com/rosberry/android/gradle/rawf/jira/RAWF.java
+++ src/main/java/com/rosberry/android/gradle/rawf/jira/RAWF.java
@@ -8,8 +8,8 @@ import java.util.List;
 
 public class RAWF {
 
-    public void doWork(String jiraUrl, String jiraLogin, String jiraToken, String projectKey, String jiraComponent, String jiraStatus,
-                       String buildNumber, String slackUrl) {
+    public void doWork(String jiraUrl, String jiraLogin, String jiraToken, String projectKey, String jiraComponent,
+                       String jiraStatus, String buildNumber, String slackUrl) {
 
         JIRAApi jiraApi = new JIRAApi(jiraUrl, jiraLogin, jiraToken);
         List<Issue> issues = jiraApi.getIssues(projectKey, jiraComponent, jiraStatus);
@@ -20,5 +20,21 @@ public class RAWF {
 
         SlackApi api = new SlackApi(slackUrl);
         api.call(slackMessage);
+    }
+
+    public String getReleaseNotesMessage(String jiraUrl, String jiraLogin, String jiraToken, String projectKey,
+                                         String jiraComponent, String jiraStatus) {
+        JIRAApi jiraApi = new JIRAApi(jiraUrl, jiraLogin, jiraToken);
+        List<Issue> issues = jiraApi.getIssues(projectKey, jiraComponent, jiraStatus);
+
+        StringBuilder message = new StringBuilder();
+        for (Issue issue : issues) {
+            message.append(issue.getKey())
+                    .append(" : ")
+                    .append(issue.getTitle())
+                    .append(".\n");
+        }
+
+        return message.toString();
     }
 }
--- src/test/java/com/rosberry/android/gradle/rawf/jira/RAWFTest.java
+++ src/test/java/com/rosberry/android/gradle/rawf/jira/RAWFTest.java
@@ -2,10 +2,12 @@ package com.rosberry.android.gradle.rawf.jira;
 
 import org.junit.Test;
 
+import java.util.logging.Logger;
+
 public class RAWFTest {
 
     private static final String slackUrl = "https://hooks.slack.com/services/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";
-    private static final String jiraUrl = "";
+    private static final String jiraUrl = "https://rosberry.atlassian.net";
     private static final String projectKey = "ALCO";
     private static final String jiraLogin = "alexey.korshun@rosberry.com";
     private static final String jiraToken = "GNs2mV6X0Qm5S9PVTbp1FA29";
@@ -16,5 +18,13 @@ public class RAWFTest {
     @Test
     public void doWork() {
         new RAWF().doWork(jiraUrl, jiraLogin, jiraToken, projectKey, jiraComponent, jiraStatus, buildNumber, slackUrl);
+    }
+
+    @Test
+    public void getReleaseNotesMessage() {
+        String message = new RAWF().getReleaseNotesMessage(jiraUrl, jiraLogin, jiraToken, projectKey,
+                jiraComponent, jiraStatus);
+        Logger.getLogger("RAWFTest").info("TEST");
+        Logger.getLogger("RAWFTest").info(message);
     }
 }
