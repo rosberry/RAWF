d40ca4ff36f4fed4fcdeef92c9d8e54b0caa19c0
Alexey Korshun <lehakorshun@gmail.com>
2019-12-27 16:40:27 +0600

    add id for Issue

--- src/main/java/com/rosberry/android/gradle/rawf/jira/model/Issue.java
+++ src/main/java/com/rosberry/android/gradle/rawf/jira/model/Issue.java
@@ -2,15 +2,23 @@ package com.rosberry.android.gradle.rawf.jira.model;
 
 public class Issue {
 
+    public static final int ID_BUG = 1;
+    public static final int ID_SUB_BUG = 11015;
+
+    private final int id;
     private final String key;
     private final String title;
     private final String type;
 
-    public Issue(String key, String title, String type) {
-
+    public Issue(int id, String key, String title, String type) {
+        this.id = id;
         this.key = key;
         this.title = title;
         this.type = type;
+    }
+
+    public int getId () {
+        return id;
     }
 
     public String getKey() {
@@ -35,9 +43,15 @@ public class Issue {
     }
 
     public static class Builder {
+        private int id;
         private String key;
         private String title;
         private String type;
+
+        public Builder setId(int id) {
+            this.id = id;
+            return this;
+        }
 
         public Builder setKey(String key) {
             this.key = key;
@@ -55,7 +69,7 @@ public class Issue {
         }
 
         public Issue createIssue() {
-            return new Issue(key, title, type);
+            return new Issue(id, key, title, type);
         }
     }
 }
